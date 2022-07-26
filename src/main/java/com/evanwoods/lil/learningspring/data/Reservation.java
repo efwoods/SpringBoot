e390149f6d385330a856a8731c160203fef788a1
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-26 13:38:14 -0400

    commit

---
+++ src/main/java/com/evanwoods/lil/learningspring/data/Reservation.java
@@ -0,0 +1,60 @@
+package com.evanwoods.lil.learningspring.data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.sql.Date;
+
+@Entity
+@Table(name="RESERVATION")
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="RESERVATION_ID")
+    private Long reservationId;
+
+    @Column(name="ROOM_ID")
+    private long roomId;
+
+    @Column(name="GUEST_ID")
+    private long guestId;
+
+    @Column(name="RES_DATE")
+    private Date reservationDate;
+
+    public long getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(long reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(long roomId) {
+        this.roomId = roomId;
+    }
+
+    public long getGuestId() {
+        return guestId;
+    }
+
+    public void setGuestId(long guestId) {
+        this.guestId = guestId;
+    }
+
+    public Date getDate() {
+        return reservationDate;
+    }
+
+    public void setDate(Date date) {
+        this.reservationDate = date;
+    }
+
+}
--- src/main/java/com/evanwoods/lil/learningspring/util/AppStartupEvent.java
+++ src/main/java/com/evanwoods/lil/learningspring/util/AppStartupEvent.java
@@ -1,7 +1,6 @@
 package com.evanwoods.lil.learningspring.util;
 
-import com.evanwoods.lil.learningspring.data.Room;
-import com.evanwoods.lil.learningspring.data.RoomRepository;
+import com.evanwoods.lil.learningspring.data.*;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -11,14 +10,21 @@ import org.springframework.stereotype.Component;
 public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
 
     private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
+    private final ReservationRepository reservationRepository;
 
-    public AppStartupEvent(RoomRepository roomRepository) {
+    public AppStartupEvent(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
         this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
+        this.reservationRepository = reservationRepository;
     }
-
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         Iterable<Room> rooms = this.roomRepository.findAll();
         rooms.forEach(System.out::println);
+        Iterable<Guest> guests = this.guestRepository.findAll();
+        guests.forEach(System.out::println);
+        Iterable<Reservation> reservations = this.reservationRepository.findAll();
+        reservations.forEach(System.out::println);
     }
 }
